163d8dff8cc4cb79c8116ded8cf8a3a67bed7521
Lennart Bernhardt <l.bernhardt@live.de>
2016-04-10 19:43:06 +0200

    init

---
+++ package.lua
@@ -0,0 +1,17 @@
+  return {
+    name = "simplerpc",
+    version = "0.0.1",
+    description = "A simple description of my little package.",
+    tags = { "lua", "lit", "luvit" },
+    license = "MIT",
+    author = { name = "Lennart Bernhardt", email = "l.bernhardt@live.de" },
+    homepage = "github.com/LennyPenny/luvitjsonrpc",
+    dependencies = {
+		"creationix/weblit@2.1.1",
+		"luvit/json@2.5.2"
+	},
+    files = {
+      "simplerpc.lua",
+	  "package.lua",
+    }
+  }
